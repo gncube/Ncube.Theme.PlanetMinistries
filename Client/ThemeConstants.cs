dae160a7c7670af71d66a13896414c888e6c1604
Gerald Ncube <gncube@gmail.com>
2026-08-28 10:08:40 +0100

    refactor(containers): synchronize container title setting keys with ContainerSettings dialog TASK-013

--- Client/ThemeConstants.cs
+++ Client/ThemeConstants.cs
@@ -5,9 +5,15 @@ public static class ThemeConstants
     public const string ThemePackageName = "Ncube.Theme.PlanetMinistries";
     public const string AssetBasePath = $"~/Themes/{ThemePackageName}";
 
+    // Site & Page Brand Settings
     public const string BrandLogoUrlSetting = "Brand_LogoUrl";
     public const string BrandHeaderStyleSetting = "Brand_HeaderStyle";
     public const string BrandFooterCopySetting = "Brand_FooterCopy";
+
+    // Container Settings Keys
+    public const string ContainerLayoutSetting = $"{ThemePackageName}:Layout";
+    public const string ContainerTitleSetting = $"{ThemePackageName}:Title";
+    public const string ContainerAnimateSetting = $"{ThemePackageName}:Animate";
 
     public static class Panes
     {
